65f335efe55a75416a136fe87e3006882ab5b7af
emilie-martin <emmy.mar@hotmail>
2020-01-26 06:29:54 -0500

    Implemented main menu

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{ 
+    void Start() {
+        GetComponent<Button>().onClick.AddListener(TaskOnClick);
+    }
+  void TaskOnClick()   {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
