aa0cf5d96744da9bf7a8d612b299562c52eca457
Daniela Venuta <44103490+daniela-venuta@users.noreply.github.com>
2020-01-25 23:41:48 -0500

    Add first iteration of spawning script and AR Sessions

---
+++ Assets/SpawnClothing.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnClothing : MonoBehaviour
+{
+     public GameObject objectPrefab;
+    public Vector3 center;
+    public Vector3 size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameObject.FindGameObjectsWithTag("clothing").Length < 3)
+        {
+            SpawnObject();  
+        } 
+    }
+
+    void OnDrawGizmosSelected() // what
+    {
+        Gizmos.color = new Color(1,0,0,0.5f);
+        Gizmos.DrawCube(center, size);
+    }
+
+    public void SpawnObject()
+    {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x/2, size.x/2),0, Random.Range(-size.z/2, size.z/2));
+        Instantiate(objectPrefab, pos, Quaternion.identity);
+        objectPrefab.tag = "clothing";
+    }
+}
