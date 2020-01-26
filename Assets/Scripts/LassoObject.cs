3f022d3ee7fc0924900e0e8e009f486468404c38
Daniela Venuta <44103490+daniela-venuta@users.noreply.github.com>
2020-01-26 03:50:37 -0500

    Add delete objects on touch

---
+++ Assets/Scripts/LassoObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LassoObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       detectTouch();
+    }
+
+    void detectTouch()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(GetComponent<Camera>())
+            {
+                 Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+                 RaycastHit hit;
+                if(Physics.Raycast(ray, out hit))
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+           
+        }
+        
+    }
+} 
